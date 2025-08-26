9ae96e28858e0b4512fea5530e4d26114fc965cc
Eclipse <Eclips77@users.noreply.github.com>
2025-08-26 11:08:21 +0300

    fix: update KAFKA_BOOTSTRAP default value to use 'kafka:9092' and refactor subscriber config to remove class encapsulation

--- publisher/app/config.py
+++ publisher/app/config.py
@@ -2,7 +2,7 @@ import os
 from typing import List
 
 
-KAFKA_BOOTSTRAP: List[str] = os.getenv("KAFKA_BOOTSTRAP", "localhost:9092").split(",")
+KAFKA_BOOTSTRAP: List[str] = os.getenv("KAFKA_BOOTSTRAP", "kafka:9092").split(",")
 KAFKA_TOPIC_INTERESTING: str = os.getenv("KAFKA_TOPIC_INTERESTING", "interesting")
 KAFKA_TOPIC_NOT_INTERESTING: str = os.getenv("KAFKA_TOPIC_NOT_INTERESTING", "not_interesting")
 DATASET_SUBSET: str = os.getenv("DATASET_SUBSET", "train")
